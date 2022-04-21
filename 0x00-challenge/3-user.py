2ff9d1ea4db8dea0bd59e49c87a8ce56a0ce553a
Thomas Lenormand <lenormand.thomas76110@gmail.com>
2022-04-21 13:00:27 +0000

    try again

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -52,7 +52,7 @@ class User():
         """
         if pwd is None or type(pwd) is not str:
             return False
-        elif self.__password is None:
+        if self.__password is None:
             return False
         return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
 
